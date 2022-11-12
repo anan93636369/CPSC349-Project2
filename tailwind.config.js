309b49b4f89b514eebeff729ccfc001c55d83e22
Mike Ball <113746603+ColdShaker@users.noreply.github.com>
2022-11-12 13:20:46 -0800

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./app/**/*.hbs"],
   theme: {
-    extend: {},
+    extend: {
+    colors: {
+        brightRed: 'hsl(12,88%,59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12,88%,95%)',
+        darkBlue: 'hsl(228,39%,23%)',
+        darkGrayishBlue: 'hsl(227,12%,13%)',
+        veryDarkBlue: 'hsl(13,100%,96%)',
+        veryPaleRed: 'hsl(13,100%,96%)',
+        veryLightGray: 'hsl(0,0%,98%)'
+
+      },
+    },
   },
   plugins: [],
 }
